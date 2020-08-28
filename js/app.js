39bf63727b8b805d1c584e6ba1867cc8e94d47fa
Rainer Brieger <hello@rainerb.com>
2020-08-27 19:30:18 -0600

    Added options html page, created on click functions for each selection

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var selection = Cookies.get("");
+
+var deepTechno = document.getElementById("deep");
+function deepClick() {
+    Cookies.set("deeptechno", "Deep Techno");
+}
+
+var industrialTechno = document.getElementById("industrial");
+function industrialClick() {
+    Cookies.set("industrialtechno", "Industrial Techno");
+}
+
+var peakTimeTechno = document.getElementById("peaktime");
+function peakTimeClick() {
+    Cookies.set("peaktimetechno", "Peak Time Techno");
+}
