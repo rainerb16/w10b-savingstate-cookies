de9acd9e42b8ed82bbc87d7af5b9f69d4a5e2d49
Rainer Brieger <hello@rainerb.com>
2020-08-28 18:28:29 -0600

    Added error/start again message when no selection is chosen

--- js/selection.js
+++ js/selection.js
@@ -1,8 +1,11 @@
 // Chosen Selection Page
 var selection = Cookies.get("techno");
-
 var chosenAnswer = document.getElementById("selection-result");
 chosenAnswer.innerHTML = selection;
+
+if(chosenAnswer.innerHTML === "undefined") {
+    chosenAnswer.innerHTML = "<h3>Oops, there was no choice made!<br> Please start again and select a genre.<h3>";
+}
 
 function removeChoice() {
   var removeOption = document.getElementById("remove-button");
